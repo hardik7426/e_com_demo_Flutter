39f98a9304df36f38f5667da570762b6c3b847dd
hardik7426 <hardikparekh7426@gmail.com>
2025-07-11 09:38:00 +0530

    new-pages

---
+++ lib/dashboard_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard"),
+      ),
+      body: Card(
+        child:Column(
+          children: [
+            Container(
+              height: 100,
+              color:Colors.red,
+            ),
+            Text("Name of Product",
+            style: TextStyle(fontWeight: FontWeight.bold,),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+              Text("40",
+              style: TextStyle(color: Colors.red),),
+              Text("39"),
+            ],)
+          ],
+        )
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:e_com_demo/dashboard_page.dart';
 import 'package:flutter/material.dart';
+
+import 'login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,65 +34,8 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: LoginPage(),
+      home: DashboardPage(),
     );
   }
 }
 
-class LoginPage extends StatefulWidget {
-  LoginPage({super.key});
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  TextEditingController userTxtCtr = TextEditingController();
-
-  TextEditingController pwdTxtCtr = TextEditingController();
-
-  String msg="Message here";
-
-  @override
-  Widget build(BuildContext context) {
-    print('from bulid method');
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(left:16,right:16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Login',
-                style: TextStyle(fontSize: 50, color: Colors.deepPurple),
-              ),
-              Text('Username'),
-              TextField(controller: userTxtCtr),
-              Text('Password'),
-              TextField(controller: pwdTxtCtr, obscureText: true),
-              ElevatedButton(
-                onPressed: () {
-                  print(
-                    "Button clicked " + userTxtCtr.text + " " + pwdTxtCtr.text,
-                  );
-                  if(userTxtCtr.text==pwdTxtCtr.text){
-                    msg = "Welcome "+userTxtCtr.text;
-                  }else{
-                    msg = "Invalid credentials";
-                  }
-                  print(msg);
-                  setState(() {
-                    
-                  });
-                },
-                child: Text('Login'),
-              ),
-              Text(msg),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
